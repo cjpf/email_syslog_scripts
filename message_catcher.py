0b4e4d5ee8a9839e5a11bd01868e9bc4a614b5c0
cjpf <cjpf@charliejuliet.net>
2020-01-07 00:36:37 -0500

    options and usage functions

---
+++ message_catcher.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# CJ Pfenninger
+# January 2020
+# This script will read an ESS syslog file and parse each new line of the file.  It writes the position of the log each time a message is recorded to keep it's place so that
+# it does not parse entries more than once.
+import sys, getopt, re, json, time
+
+# Process Arguments
+def processArgs():
+    if(len(sys.argv) == 1):
+        usage(2)
+    logfile = ''
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hl:')
+    except getopt.GetoptError:
+        usage(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--h"):
+            usage(0)
+        elif opt in ("-l", "--log"):
+            logfile = arg
+        else:
+            usage(0)
+    return logfile
+
+# Prints a usage helper and exits
+def usage(code):
+    if type(code) != type(1):
+        code = 2
+    print('message_catcher.py -l <logfile>')
+    sys.exit(code)
+
+
+# MAIN
+log = open(processArgs(), "r")
+
+data = log.readlines()
+
+
+while 1:
+    time.sleep(5)
+    pointer = log.tell()
+    line = log.readline()
+    if not line:
+        log.seek(pointer)
+    else:
+        data = re.findall('\{.*\}', line)
+        data = json.loads(data[0])
+        print(data)
+
