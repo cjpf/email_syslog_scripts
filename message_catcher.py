cab345514ee61a6627bcfd62e2a662065c85ec36
cj pfenninger <cjpf@charliejuliet.net>
2020-01-07 23:10:15 -0500

    refactored entire script
    changed logfile to logpath
    started closing the file like a goodboy
    changed /tmp/message_catcher_position to /tmp/mcpos
    added a check for the existence of this tmp file
    tightened up flow control and optimized

--- message_catcher.py
+++ message_catcher.py
@@ -9,7 +9,7 @@ import sys, getopt, re, json, time, os
 def processArgs():
     if(len(sys.argv) == 1):
         usage(2)
-    logfile = ''
+    logpath = ''
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hl:')
     except getopt.GetoptError:
@@ -18,64 +18,65 @@ def processArgs():
         if opt in ("-h"):
             usage(0)
         elif opt in ("-l"):
-            logfile = arg
+            logpath = arg
         else:
             usage(0)
-    return logfile
+    return logpath
 
 # Prints a usage helper and exits
 def usage(code):
     if type(code) != type(1):
         code = 2
     print('Invalid arguments.')
-    print('usage: message_catcher.py -l <logfile>')
+    print('usage: message_catcher.py -l <logpath>')
     sys.exit(code)
 
 # Writes the position of the log file 
 def writePosition(posfile, pos):
     with open(posfile, 'w') as file:
         file.write(str(pos))
+        file.close()
 
 # Reads the previous position of the log file
 def getPosition(posfile):
     with open(posfile, 'r') as file:
-        return int(file.read())
-
+        value = int(file.read())
+        file.close()
+        return value
 
 # SETUP
-# Process arguments to get the logfile 
-logfile = processArgs()
-# Open the logfile
-log = open(logfile, "r")
+# Process arguments to get the log path and init temp file path
+logpath = processArgs()
+positionfile = '/tmp/mcpos'
 
-# Get the previously read position of the logfile or start at the top
-positionfile = '/tmp/message_catcher_position'
+# Create and init temp file if DNE
+if not os.path.isfile(positionfile):
+    writePosition(positionfile, 0)
 
-if os.stat(positionfile).st_size == 0:
-    position = 0
-else:
-    position = getPosition(positionfile)
-
-# Move log file to the position
-log.seek(position)
+# Get position from temp file
+position = getPosition(positionfile)
 
 # MAIN LOOP
 while 1:
     # Parsing Interval
     time.sleep(0.1)
-    # Update the previous position in system file
-    writePosition(positionfile, position)
-    # Get new position
-    position = log.tell()
+    # Open the log
+    log = open(logpath, 'r')
+    # Seek to position
+    log.seek(position)
     # Get new line
     line = log.readline()
-    # If line is empty, return to position and wait
+    # Get new position
+    position = log.tell()
+    log.close()
     if not line:
-        log.seek(position)
         time.sleep(3)
     # If there is a line of data, parse out the JSON and decode it for storage.
     else: 
         data = re.findall('\{.*\}', line)
         data = json.loads(data[0])
         print(json.dumps(data, indent=2))
+        # Update the position in temp file
+        writePosition(positionfile, position)
+
 
