fa2c34ff3fe00efdeb122e892b35ba04788dda1b
cj pfenninger <cjpf@charliejuliet.net>
2020-01-15 13:26:11 -0500

    refactor
    refactored so it does not make prints to stdout any longer, and the position file has been deprecated

--- mail_parser.py
+++ mail_parser.py
@@ -13,8 +13,6 @@ place so that it does not parse entries more than once.
 import sys
 import re
 import json
-import time
-import os
 import argparse
 import mail_store
 
@@ -30,72 +28,21 @@ def process_args():
     return parser.parse_args(sys.argv[1:])
 
 
-def write_position(posfile, pos):
-    """Writes the position of the log file"""
-    with open(posfile, 'w') as file:
-        file.write(str(pos))
-        file.close()
-
-
-def get_position(posfile):
-    """Reads the previous position of the log file"""
-    with open(posfile, 'r') as file:
-        value = int(file.read())
-        file.close()
-        return value
-
-
-def get_line(datafile, position):
-    """Returns the next line in the file"""
-    with open(datafile, 'r') as file:
-        file.seek(position)
-        line = file.readline()
-        position = file.tell()
-        file.close()
-    return line, position
-
-
 def main():
     """."""
     args = process_args()
-    positionfile = '/tmp/mcpos'
-    if not os.path.isfile(positionfile):
-        write_position(positionfile, 0)
-
-    position_proc = get_position(positionfile)
 
     mail_store.build_store()
 
-    while 1:
-        data, position_proc = get_line(args.log[0], position_proc)
+    with open(args.log[0], 'r') as log:
+        data = log.readline()
+
         if not data:
-            time.sleep(15)
-
-            print('Displaying MESSAGE TABLE DATA')
-            time.sleep(2)
-            i = 1
-            for row in mail_store.read_messages():
-                print(i, ': ', row)
-                i += 1
-
-            print('Displaying RECIPIENTS TABLE DATA')
-            time.sleep(2)
-            i = 1
-            for row in mail_store.read_recipients():
-                print(i, ': ', row)
-                i += 1
-
-            print('Displaying ATTACHMENTS TABLE DATA')
-            time.sleep(2)
-            i = 1
-            for row in mail_store.read_attachments():
-                print(i, ': ', row)
-                i += 1
+            exit
         else:
             data = re.findall(r'\{.*\}', data)
             data = json.loads(data[0])
             mail_store.store(data)
-            write_position(positionfile, position_proc)
 
 
 if __name__ == '__main__':
